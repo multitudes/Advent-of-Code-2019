5e30bcf6668244f3a4fb189be0904f0f69f44b43
laurent <wrmultitudes@gmail.com>
2019-12-07 15:27:57 +0100

    review strings in day 3

--- Advent Of Code 2019.playground/Pages/Day 3 -Crossed Wires.xcplaygroundpage/Contents.swift
+++ Advent Of Code 2019.playground/Pages/Day 3 -Crossed Wires.xcplaygroundpage/Contents.swift
@@ -35,10 +35,10 @@ for i in 0..<2 {
 let wire = wires[i].components(separatedBy: ",") // get two arrays of [String] like ["D323",...]
     // get two arrays of tuples wireTuples[0] is
 wireTuples[i] = wire.map {  (str: String) -> (String, Int) in
-                            let firstIndex = str.index(str.startIndex, offsetBy: 0)
+                            let firstIndex = str.startIndex
                             let a = str[firstIndex]
                             let secondIndex = str.index(after: str.startIndex)
-                            let lastIndex = str.index(str.endIndex, offsetBy: 0)
+                            let lastIndex = str.endIndex
                             let range = secondIndex..<lastIndex
                             let b = Int(str[range])
                             return  (String(a), b ?? 0)}
