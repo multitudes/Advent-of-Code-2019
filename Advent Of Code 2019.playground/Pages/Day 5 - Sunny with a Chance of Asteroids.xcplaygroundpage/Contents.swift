65e0d0e5792a9c9633173f5c9351fb29ede76e66
laurent <wrmultitudes@gmail.com>
2019-12-08 17:34:14 +0100

    start day 5

---
+++ Advent Of Code 2019.playground/Pages/Day 5 - Sunny with a Chance of Asteroids.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+/*:
+# Day 5 - Sunny with a Chance of Asteroids
+
+[See it on github](https://github.com/multitudes/Advent-of-Code-2019)
+ 
+You're starting to sweat as the ship makes its way toward Mercury. The Elves suggest that you get the air conditioner working by upgrading your ship computer to support the Thermal Environment Supervision Terminal.
+[...]
+Finally, the program will output a diagnostic code and immediately halt. This final output isn't an error; an output followed immediately by a halt means the program finished. If all outputs were zero except the diagnostic code, the diagnostic program ran successfully. After providing 1 to the only input instruction and passing all the tests, what diagnostic code does the program produce?
+ 
+[Next >](@next)
+*/
+import Foundation
