c597788246d10d8d7aa0063fb49e731b54ee89a2
laurent <wrmultitudes@gmail.com>
2019-12-10 20:27:14 +0100

    day8

---
+++ Advent Of Code 2019.playground/Pages/Day 8 - Space Image Format.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+//: [Previous](@previous)
+/*:
+# Day 8: Space Image Format
+
+ [See it on github](https://github.com/multitudes/Advent-of-Code-2019)
+   
+The Elves' spirits are lifted when they realize you have an opportunity to reboot one of their Mars rovers, and so they are curious if you would spend a brief sojourn on Mars. You land your ship near the rover.
+
+When you reach the rover, you discover that it's already in the process of rebooting! It's just waiting for someone to enter a BIOS password. The Elf responsible for the rover takes a picture of the password (your puzzle input) and sends it to you via the Digital Sending Network.
+
+Unfortunately, images sent via the Digital Sending Network aren't encoded with any normal encoding; instead, they're encoded in a special Space Image Format. None of the Elves seem to remember why this is the case. They send you the instructions to decode it.
+ [...]
+ 
+ The image you received is 25 pixels wide and 6 pixels tall.
+
+ To make sure the image wasn't corrupted during transmission, the Elves would like you to find the layer that contains the fewest 0 digits. On that layer, what is the number of 1 digits multiplied by the number of 2 digits?
+ 
+: [Next](@next)
+*/
+import Foundation
