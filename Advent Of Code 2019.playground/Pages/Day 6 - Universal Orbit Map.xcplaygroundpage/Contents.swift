12dc613f126ce0d4c95640888c78cad601879318
laurent <wrmultitudes@gmail.com>
2019-12-10 08:59:56 +0100

    starting day 6 pt2

--- Advent Of Code 2019.playground/Pages/Day 6 - Universal Orbit Map.xcplaygroundpage/Contents.swift
+++ Advent Of Code 2019.playground/Pages/Day 6 - Universal Orbit Map.xcplaygroundpage/Contents.swift
@@ -18,27 +18,27 @@ var map = input.components(separatedBy: "\n").filter { $0 != "" }
 // class SpaceObject is in separate file. Class contains properties collecting the parent and children of instance
 
 // planet is array of two orbiting bodies like in input a line "H8Y)CGB" gets to planet[0] = "H8Y" and planet[1] is "CGB"
-var planet : [String] = []
+var planets : [String] = []
 // create a dictionary. key is the planet I got and the value is the object
 var allOrbitingObj = [String : SpaceObject]()
 
 // here I fill all values in my dictionary
 for i in 0..<map.count {
     // planet will be an array of two planets
-    planet = map[i].components(separatedBy: ")")
+    planets = map[i].components(separatedBy: ")")
     // need check if child exists in allOrbitingObj dict, key for the dic is like planet[..] "CGB" and if not create and add it
     // to add to dic need to do dict[key] = val or allOrbitingObj[planet[1]] = objChild creating the objChild on the line before
-    if  allOrbitingObj[planet[1]] == nil {
-        let objChild = SpaceObject(value: planet[1])
-        allOrbitingObj[planet[1]] = objChild
+    if  allOrbitingObj[planets[1]] == nil {
+        let objChild = SpaceObject(value: planets[1])
+        allOrbitingObj[planets[1]] = objChild
     }
     // check if parent exists and if not create it
-    if  allOrbitingObj[planet[0]] == nil {
-        let objParent = SpaceObject(value: planet[0])
-        allOrbitingObj[planet[0]] = objParent
+    if  allOrbitingObj[planets[0]] == nil {
+        let objParent = SpaceObject(value: planets[0])
+        allOrbitingObj[planets[0]] = objParent
     }
     // check if parent exists and if so add the child
-    if let objParent = allOrbitingObj[planet[0]], let objChild = allOrbitingObj[planet[1]] {
+    if let objParent = allOrbitingObj[planets[0]], let objChild = allOrbitingObj[planets[1]] {
         objParent.add(orbitingObject: objChild)
     }
 }
