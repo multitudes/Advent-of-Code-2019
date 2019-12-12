c9555c6afca687632d460ade9ee209f1f6446672
laurent <wrmultitudes@gmail.com>
2019-12-12 20:04:05 +0100

    day 10

--- Advent Of Code 2019.playground/Pages/Day 10 - Monitoring Station.xcplaygroundpage/Contents.swift
+++ Advent Of Code 2019.playground/Pages/Day 10 - Monitoring Station.xcplaygroundpage/Contents.swift
@@ -34,11 +34,11 @@ struct Universe {
     var asteroidMap: [String]
     lazy var xBounds = asteroidMap[0].count
     lazy var yBounds = asteroidMap.count
-   
+    var monitoringStation = Asteroid()
 }
 struct Asteroid {
-    
-    //var sightings
+    var location: (xPos: Int, yPos: Int) = (xPos : 0, yPos: 0)
+    //lazy var sightings: Int =
 }
 //enum Position: String {
 //    case asteroid = "#", noAsteroid = "."
@@ -47,28 +47,39 @@ struct Asteroid {
 //struct coordinates {
 //    let position: (Int, Int)
 //}
+// create polar coordinates for part2
+var polarCoordinates:[(radius: Double, angle :Double)] = []
 
+//from one asteroid at pos x y , I return the number of unique asteroids(angles) I see and return a set of unique values
 func checkAsteroid(coordinates: (xPos: Int, yPos: Int), universe: Universe) -> Set<Double> {
     var angles = Set<Double>()
+  
     for y in 0..<space.yBounds {
         for x in 0..<space.xBounds{
             if space.asteroidMap[y][x] == "#" {
             // from rwenderlich For this specific problem, instead of using atan(), it’s simpler to use the function atan2(_:_:), which takes the x and y components as separate parameters, and correctly determines the overall rotation angle.
-            var angle = atan2(Double(y - coordinates.yPos) , Double(x - coordinates.xPos))
-            angles.insert( angle )
+                let angle: Double = atan2(Double(y - coordinates.yPos) , Double(x - coordinates.xPos))
+                angles.insert( angle )
+                let radius: Double = getRadius(Double(y - coordinates.yPos) , Double(x - coordinates.xPos))
+                polarCoordinates.append((radius: radius, angle: angle))
             } else { continue }
         }
     }
     return angles
 }
 
-var space = Universe(asteroidMap: asteroidMap)
+func getRadius(_ a: Double, _ b: Double) -> Double {
+    return (a * a + b * b).squareRoot()
+}
 
+var space = Universe(asteroidMap: asteroidMap)
+//looping to every corner of my universe. I check from every asteroid I land which one is the one with the most visible asteroids
 var location: (xPos: Int, yPos: Int) = (xPos : 0, yPos: 0)
 var sightings = 0
 for y in 0..<space.yBounds {
     for x in 0..<space.xBounds{
         if space.asteroidMap[y][x] == "#" {
+            
             let thisAsteroidSightings = checkAsteroid(coordinates: (xPos : x, yPos: y), universe: space).count
             if thisAsteroidSightings > sightings {
                 sightings = thisAsteroidSightings
@@ -78,3 +89,8 @@ for y in 0..<space.yBounds {
     }
 }
 print("\nsightings: \(sightings) for asteroid at coordinates \(location)")
+polarCoordinates.sort(by: {$0.angle < $1.angle})
+print(polarCoordinates.count)
+//let test = atan2(Double(2) , Double(0))
+
+
