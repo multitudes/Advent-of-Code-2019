aa4d54a1d19bc898643db9e0b024ec69d18a4e85
laurent <wrmultitudes@gmail.com>
2020-01-01 15:53:34 +0100

    check everything still works till day 7

--- Advent Of Code 2019.playground/Pages/Day 6 - Universal Orbit Map.xcplaygroundpage/Contents.swift
+++ Advent Of Code 2019.playground/Pages/Day 6 - Universal Orbit Map.xcplaygroundpage/Contents.swift
@@ -6,6 +6,112 @@
    
 You've landed at the Universal Orbit Map facility on Mercury. Because navigation in space often involves transferring between orbits, the orbit maps here are useful for finding efficient routes between, for example, you and Santa. You download a map of the local orbits (your puzzle input).
 
+Except for the universal Center of Mass (COM), every object in space is in orbit around exactly one other object. An orbit looks roughly like this:
+```
+                  \
+                   \
+                    |
+                    |
+AAA--> o            o <--BBB
+                    |
+                    |
+                   /
+                  /
+```
+
+ In this diagram, the object BBB is in orbit around AAA. The path that BBB takes around AAA (drawn with lines) is only partly shown. In the map data, this orbital relationship is written AAA)BBB, which means "BBB is in orbit around AAA".
+
+Before you use your map data to plot a course, you need to make sure it wasn't corrupted during the download. To verify maps, the Universal Orbit Map facility uses orbit count checksums - the total number of direct orbits (like the one shown above) and indirect orbits.
+
+Whenever A orbits B and B orbits C, then A indirectly orbits C. This chain can be any number of objects long: if A orbits B, B orbits C, and C orbits D, then A indirectly orbits D.
+
+For example, suppose you have the following map:
+
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+
+ Visually, the above map of orbits looks like this:
+```
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I
+```
+ 
+ In this visual representation, when two objects are connected by a line, the one on the right directly orbits the one on the left.
+
+Here, we can count the total number of orbits as follows:
+
+D directly orbits C and indirectly orbits B and COM, a total of 3 orbits.
+L directly orbits K and indirectly orbits J, E, D, C, B, and COM, a total of 7 orbits.
+COM orbits nothing.
+The total number of direct and indirect orbits in this example is 42.
+
+What is the total number of direct and indirect orbits in your map data?
+
+Your puzzle answer was 245089.
+
+--- Part Two ---
+
+Now, you just need to figure out how many orbital transfers you (YOU) need to take to get to Santa (SAN).
+
+You start at the object YOU are orbiting; your destination is the object SAN is orbiting. An orbital transfer lets you move from any object to an object orbiting or orbited by that object.
+
+For example, suppose you have the following map:
+
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN
+Visually, the above map of orbits looks like this:
+
+ ```
+                          YOU
+                         /
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I - SAN
+```
+ In this example, YOU are in orbit around K, and SAN is in orbit around I. To move from K to I, a minimum of 4 orbital transfers are required:
+
+K to J
+J to E
+E to D
+D to I
+Afterward, the map of orbits looks like this:
+```
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I - SAN
+                 \
+                  YOU
+```
+ What is the minimum number of orbital transfers required to move from the object YOU are orbiting to the object SAN is orbiting? (Between the objects they are orbiting - not between YOU and SAN.)
+
+
 : [Next](@next)
 */
 
@@ -16,7 +122,49 @@ var input = getInput(inputFile: "input6", extension: "txt")
 var map = input.components(separatedBy: "\n").filter { $0 != "" }
 
 // class SpaceObject is in separate file. Class contains properties collecting the parent and children of instance
+extension SpaceObject: CustomStringConvertible {
+  //This is a computed property. Mapping will be recursive!
+    public var description: String {
+        var text = "\(name)"
+        if !orbitingObjects.isEmpty {
+          text += " {" + orbitingObjects.map { $0.description }.joined(separator: ", ") + "} "
+        }
+        return text
+    }
+    // this will search for the object name and return the object or one of its children
+    public func search(name: String) -> SpaceObject? {
+        if name == self.name {
+            return self
+        }
+        for orbitingObject in orbitingObjects {
+            if let found = orbitingObject.search(name: name) {
+            return found
+            }
+            }
+        return nil
+    }
 
+}
+
+public class SpaceObject : Hashable {
+    public var name: String
+    public var orbitingObjects:[SpaceObject] = []
+    public weak var parentObject: SpaceObject? // this is the parent. not every object got a parent. weak to avoid retain cycles.
+    public init(name: String) {
+        self.name = name
+    }
+    public func add(orbitingObject: SpaceObject) {
+        orbitingObjects.append(orbitingObject)
+        orbitingObject.parentObject = self //once I add an orbiting object (child) then I become a center object (parent) of the child
+    }
+    // this is tricking swift to make sets of spaceobjects. The class needs to conform to hashable and equatable.
+    public static func == (lhs: SpaceObject, rhs: SpaceObject) -> Bool {
+        return lhs.name == rhs.name
+    }
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+}
 // planet is array of two orbiting bodies like in input a line "H8Y)CGB" gets to planet[0] = "H8Y" and planet[1] is "CGB"
 var planets : [String] = []
 // create a dictionary. key is the planet I got and the value is the object
--- Advent Of Code 2019.playground/Pages/day 4 - Secure Container.xcplaygroundpage/Contents.swift
+++ Advent Of Code 2019.playground/Pages/day 4 - Secure Container.xcplaygroundpage/Contents.swift
@@ -3,8 +3,37 @@
 # Day 4: Secure Container
 
 [See it on github](https://github.com/multitudes/Advent-of-Code-2019)
-You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out. &nbsp;
-How many different passwords within the range given in your puzzle input meet these criteria?
+
+ You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out.
+
+ However, they do remember a few key facts about the password:
+
+ It is a six-digit number.
+ The value is within the range given in your puzzle input.
+ Two adjacent digits are the same (like 22 in 122345).
+ Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+ Other than the range rule, the following are true:
+
+ 111111 meets these criteria (double 11, never decreases).
+ 223450 does not meet these criteria (decreasing pair of digits 50).
+ 123789 does not meet these criteria (no double).
+ How many different passwords within the range given in your puzzle input meet these criteria?
+
+ Your puzzle answer was 511.
+
+ --- Part Two ---
+
+ An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
+
+ Given this additional criterion, but still ignoring the range rule, the following are now true:
+
+ 112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+ 123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+ 111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+ How many different passwords within the range given in your puzzle input meet all of the criteria?
+
+ Your puzzle answer was 316.
+ 
 [Next >](@next)
 */
 import Foundation
@@ -29,7 +58,8 @@ outerloop: for i in low...high {
         if digits[j] >= digits[j-1] { flag = true } else { continue outerloop }
     }
     if double == false { continue outerloop }
-    print(digits)
+    // debug
+    //print(digits)
     passwords.append(i)
 }
 let solution1 = passwords.count
